8d65bfc4bfab798ec54d2be49e72ed54a2f6c481
Ethan G <ethanhg.pnw@gmail.com>
2026-05-11 21:16:38 +0000

    lab8_p1 complete

--- lab8_p1.py
+++ lab8_p1.py
@@ -12,38 +12,46 @@ class Table:
     data: List[Optional[Tuple[int, str]]]
 
 def hash_fn(entry: Tuple[int, str], size: int) -> int:
-    
     return entry[0] % size 
-
     
 
 def insert(table: Table, entry: Tuple[int, str]) -> Table:
-    index = hash_fn(table)
-    if table[index] == None:
-        table[index] = entry
+    """
+    Inserts a (key, value) entry using linear probing.
+    Overwrites if key already exists.
+    Raises RuntimeError if the table is full.
+    """
+    index = hash_fn(entry, table.size)
+    if table.data[index] is None:
+        table.data[index] = entry
+        return table
 
     if table[index] > table.size:
-        raise RuntimeError
+        raise RuntimeError("Hash Table Full")
+        
+    
+
+def delete(table: Table, key: int) -> Table:
+    """
+    Deletes an entry with the given key by setting the slot to None.
+    """
+    index = key % table.size
+    if table.data[index] != None:
+        table.data[index] = None
         
     return table
     
 
-def delete(table: Table, key: int) -> Table:
-    index = hash_fn(table)
-    if table[index] != None:
-        table[index] = None
-        
-    return Table
-    
-
 def get(table: Table, key: int) -> Optional[str]:
+   
     """
     Retrieves the value associated with the given key using linear probing.
     Returns None if the key is not found.
     """
     
-    index = hash_fn(key, table)
-    if table[index] != None:
-        return table[index]
+    index = key % table.size
+    if table.data[index] != None:
+        return table.data[index][1]
+    
     return None
     
