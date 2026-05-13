9f6525f8e6e05c2c3594d7332c4b4bcf6e9046ca
Ethan G <ethanhg.pnw@gmail.com>
2026-05-13 21:06:36 +0000

    lab8_p2 complete

--- lab8_p2.py
+++ lab8_p2.py
@@ -4,10 +4,7 @@ from typing import Optional, Tuple
 TOMBSTONE = (100000, "")
 
 def hash_fn(entry: Tuple[int, str], size: int) -> int:
-    """
-    Hashes the key of an entry using modulo.
-    """
-    pass
+    return entry[0] % size
 
 def insert(table: Table, entry: Tuple[int, str]) -> Table:
     """
@@ -15,13 +12,33 @@ def insert(table: Table, entry: Tuple[int, str]) -> Table:
     Reuses tombstone slots if found.
     Overwrites if the key already exists.
     """
-    pass
+    index = hash_fn(entry, table.size)
+    start = index
+
+    if table.data[index] != None:
+        index = (index + 1) % table.size
+
+    while index != start:
+      if table.data[index] == None:
+        table.data[index] = entry
+        break
+      index = (index + 1) % table.size
+  
+    table.data[index] = entry
+
+    return table
 
 def delete(table: Table, key: int) -> Table:
     """
     Deletes the entry with the given key by marking the slot with TOMBSTONE.
     """
-    pass
+    index = key % table.size
+    
+    if table.data[index] != None:
+        table.data[index] = TOMBSTONE
+        
+    return table
+    
 
 def get(table: Table, key: int) -> Optional[str]:
     """
@@ -29,4 +46,19 @@ def get(table: Table, key: int) -> Optional[str]:
     Skips over tombstones.
     Returns None if not found.
     """
-    pass
+    index = key % table.size
+    start = index
+
+    while True:
+        entry = table.data[index]
+
+        if entry is None:
+            return None
+
+        if entry != TOMBSTONE and entry[0] == key:
+            return entry[1]
+
+        index = (index + 1) % table.size
+
+        if index == start:
+            return None
