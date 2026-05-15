f3177c4b870df08b125cd55635c7e1f881853717
Ethan G <ethanhg.pnw@gmail.com>
2026-05-15 17:03:13 +0000

    complete

--- lab8_p3.py
+++ lab8_p3.py
@@ -6,4 +6,12 @@ def rehash(table: Table, new_size: int) -> Table:
     Creates a new table of larger size and reinserts all
     valid (non-None, non-TOMBSTONE) entries from the original table.
     """
-    pass
+    new_table = Table(new_size, [None] * new_size)
+
+    for item in table.data:
+        if item != None and item != TOMBSTONE:
+
+            insert(new_table, item)
+    
+    return new_table
+
